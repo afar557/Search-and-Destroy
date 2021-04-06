33414caa73c8dcfe000a7ebfb3641254f9d06c3f
Asma Makhdoom <asmamakhdoom@Asmas-MBP.lan>
2021-04-06 17:17:00 -0400

    finished improved agent

---
+++ Code3_af704_nhe12_aam355/improvedAgent.py
@@ -0,0 +1,65 @@
+from collections import deque
+import random
+from agent1 import manhattanDistance, getFNR, getTotalProb, normalizeCells, updateBelief
+
+def agent3(grid, target):
+    dimension = len(grid)
+    # Number of searches
+    searches = 0
+    # Distance traveled
+    totalDistance = 0
+
+    # Calculate the initial probability of each cell
+    initProb = 1 / (dimension*dimension)
+
+    # Create the belief matrix
+    beliefMatrix = [[initProb for i in range(dimension)] for j in range(dimension)]
+    # print("Belief Matrix ->")
+    # for x in beliefMatrix:
+    #     print(x)
+
+    # Create queue to store cells
+    queue = deque()
+    # Pick random cell to enqueue
+    xValue = random.randint(0,dimension-1)
+    yValue = random.randint(0,dimension-1)
+    # Enqueue random coordinates
+    queue.append((xValue,yValue))
+
+    # Iterate through the queue
+    while queue:
+        x,y = queue.popleft()
+
+        # increments number of searhces
+        searches+=1
+
+        # Check if the dequeued cell is the target
+        if (x,y) == target:
+            # Generate a random number between 
+            rand = random.uniform(0,1)
+            if rand>getFNR(grid[x][y]):
+                print("Success")
+                print("Searches is ->", searches)
+                print("Total Distance is ->", totalDistance)
+                return totalDistance+searches
+
+        # Update the belief matrix
+        beliefMatrix = updateBelief(beliefMatrix, (x,y), grid)
+
+        # caluculate agent 2 probability
+        maxProb = -1
+        maxInd = None
+        for i in range(len(beliefMatrix)):
+            for j in range(len(beliefMatrix)):
+                distance = manhattanDistance((i,j), (x,y))
+                if distance == 0:
+                    continue
+                curr = ( (1 - getFNR(grid[i][j])) * beliefMatrix[i][j] ) / distance
+                if curr > maxProb or (curr == maxProb and manhattanDistance(maxInd, (x,y)) > distance):
+                    maxProb = curr
+                    maxInd = (i,j)
+
+        # Pick min from belief matrix and enqueue it
+        queue.append(maxInd)
+
+        totalDistance += manhattanDistance((x,y), maxInd)
--- Code3_af704_nhe12_aam355/main.py
+++ Code3_af704_nhe12_aam355/main.py
@@ -2,18 +2,20 @@ from generateBoard import generateBoard, assignTarget
 from visualizeBoard import visualizeBoard
 from agent1 import agent1
 from agent2 import agent2
+from improvedAgent import agent3
 
 def main():
-    dimension = 5
+    dimension = 50
     grid = generateBoard(dimension)
-    for x in grid:
-        print(x)
+    # for x in grid:
+    #     print(x)
 
     target = assignTarget(dimension)
-    print("target is", target)
+    # print("target is", target)
 
     print("Final score for agent1 is ->", agent1(grid, target))
     print("Final score for agent2 is ->", agent2(grid, target))
+    print("Final score for agent3 is ->", agent3(grid, target))
 
     visualizeBoard(grid, "tester", target)
 if __name__ == "__main__":
