fc1c2a09b7040b10fc0dff6feabdb2991aa2db3b
Asma Makhdoom <asmamakhdoom@Asmas-MBP.lan>
2021-04-08 19:09:34 -0400

    updated latex

--- Code3_af704_nhe12_aam355/main.py
+++ Code3_af704_nhe12_aam355/main.py
@@ -3,32 +3,39 @@ from visualizeBoard import visualizeBoard
 from agent1 import agent1
 from agent2 import agent2
 from improvedAgent import agent3
+from movingTarget import agent1EC, agent2EC, agent3EC
 
 def main():
-    osum1 = 0
-    osum2 = 0
-    osum3 = 0
-    for i in range(10):
-        sum1 = 0
-        sum2 = 0
-        sum3 = 0
-        print("map:",i)
-        dimension = 50
-        grid = generateBoard(dimension)
-        for i in range(10):
-            print("iteration:",i)
-            target = assignTarget(dimension)
+    # osum1 = 0
+    # osum2 = 0
+    # osum3 = 0
+    # for i in range(10):
+    #     sum1 = 0
+    #     sum2 = 0
+    #     sum3 = 0
+    #     print("map:",i)
+    #     dimension = 50
+    #     grid = generateBoard(dimension)
+    #     for i in range(10):
+    #         print("iteration:",i)
+    #         target = assignTarget(dimension)
         
-            sum1 += agent1(grid, target)
-            sum2 += agent2(grid, target)
-            sum3 += agent3(grid, target)
-        osum1 += sum1/10
-        osum2 += sum2/10 
-        osum3 += sum3/10
-    print("Avg score for agent1 is ->", osum1/10)
-    print("Avg score for agent2 is ->", osum2/10)
-    print("Avg score for agent3 is ->", osum3/10)
+    #         sum1 += agent1(grid, target)
+    #         sum2 += agent2(grid, target)
+    #         sum3 += agent3(grid, target)
+    #     osum1 += sum1/10
+    #     osum2 += sum2/10 
+    #     osum3 += sum3/10
+    # print("Avg score for agent1 is ->", osum1/10)
+    # print("Avg score for agent2 is ->", osum2/10)
+    # print("Avg score for agent3 is ->", osum3/10)
 
-    visualizeBoard(grid, "tester", target)
+    dimension = 10
+    grid = generateBoard(dimension)
+    target = assignTarget(dimension)
+    print("SCORE:",agent1EC(grid,target))
+    print("SCORE:",agent2EC(grid,target))
+    print("SCORE:",agent3EC(grid,target))
+    # visualizeBoard(grid, "tester", target)
 if __name__ == "__main__":
     main()
